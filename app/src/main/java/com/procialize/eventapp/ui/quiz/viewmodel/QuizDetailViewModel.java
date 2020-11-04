1b1a82c0ea618aa85bf436ce36c7922490391787
SnehaDZ <38002121+SnehaDZ@users.noreply.github.com>
2020-11-04 22:49:10 +0530

    Quiz Module

--- app/src/main/java/com/procialize/eventapp/ui/quiz/viewmodel/QuizDetailViewModel.java
+++ app/src/main/java/com/procialize/eventapp/ui/quiz/viewmodel/QuizDetailViewModel.java
@@ -5,12 +5,14 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.procialize.eventapp.ui.quiz.model.QuizListing;
+import com.procialize.eventapp.ui.quiz.model.QuizSubmit;
 import com.procialize.eventapp.ui.quiz.networking.QuizDetailRepository;
 
 public class QuizDetailViewModel extends ViewModel {
 
     private QuizDetailRepository quizRepository = QuizDetailRepository.getInstance();
     MutableLiveData<QuizListing> quizData = new MutableLiveData<>();
+    MutableLiveData<QuizSubmit> quizsubit = new MutableLiveData<>();
 
     public void getQuizList(String token, String event_id) {
 
@@ -18,8 +20,18 @@ public class QuizDetailViewModel extends ViewModel {
         quizData = quizRepository.getQuizList(token, event_id);
     }
 
+    public void quizSubmit(String token, String event_id, String quiz_question_id, String quiz_options_id) {
+
+        quizRepository = QuizDetailRepository.getInstance();
+        quizsubit = quizRepository.quizSubmit(token, event_id, quiz_question_id, quiz_options_id);
+    }
+
     public LiveData<QuizListing> getQuizList() {
         return quizData;
+    }
+
+    public LiveData<QuizSubmit> quizSubmit() {
+        return quizsubit;
     }
 
 }
